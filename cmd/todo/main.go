168834ec9844a795f0f07ac9a87761c64c36ba31
Ben Sabler <ben.sabler@velocityprint.com>
2026-01-21 09:45:31 -0500

    implemented add task flag and allow for args from STDIN, updated testing

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/bensabler/todo/internal/todo"
 )
@@ -21,7 +24,7 @@ func main() {
 	}
 
 	// Parsing command line flags
-	task := flag.String("task", "", "Task to be included in the ToDo list")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
@@ -46,13 +49,6 @@ func main() {
 	case *list:
 		fmt.Print(l)
 
-		// // List current to do items
-		// for _, item := range *l {
-		// 	if !item.Done {
-		// 		fmt.Println(item.Task)
-		// 	}
-		// }
-
 	case *complete > 0:
 		// Complete the given item
 		if err := l.Complete(*complete); err != nil {
@@ -66,15 +62,31 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		// Add a new task
-		l.Add(*task)
+	case *add:
+		// When any arguments (excluding flags) are provided, they will be
+		// used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
+	// case *task != "":
+	// 	// Add a new task
+	// 	l.Add(*task)
+
+	// 	// Save the new list
+	// 	if err := l.Save(todoFileName); err != nil {
+	// 		fmt.Fprintln(os.Stderr, err)
+	// 		os.Exit(1)
+	// 	}
 
 	default:
 		// Invalid flag provided
@@ -83,4 +95,24 @@ func main() {
 
 	}
 
+}
+
+// getTask function decides where to get the description for a new
+// task from: arguments or STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
